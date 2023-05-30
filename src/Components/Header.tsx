e8beeb04d93ed4036aca447fd637e9757fc92b87
davidgordon12 <davidgordon12@live.ca>
2023-05-30 18:17:20 -0400

    Header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="m-header">
+        <h3 className="m-header-title">MatchaIO</h3>
+    </div>
+  )
+}
+
+export default Header;
